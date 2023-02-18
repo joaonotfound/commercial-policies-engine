7b5041ead7ec1b3ac94aa164d66dfc76f93d223b
joaonotfound <joaonotfound@protonmail.com>
2023-02-18 18:46:30 -0300

    setup prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier',
+  ],
+  rules: {},
+}
