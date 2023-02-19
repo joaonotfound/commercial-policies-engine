9f4a5e7547b5e1571fc9d355c3baaa8cb7cdf6a6
joaonotfound <joaonotfound@protonmail.com>
2023-02-19 15:04:44 -0300

    feat: add submit button to login page

--- pages/login.vue
+++ pages/login.vue
@@ -11,7 +11,13 @@ const password = ref<string | null>(null)
 
 const passwordState = computed(() => usePasswordValidator(password.value))
 const usernameState = computed(() => useUsernameValidator(username.value))
-
+const validForm = computed(
+  () =>
+    username.value != null &&
+    password.value != null &&
+    (passwordState.value == null || passwordState.value.level === 'success') &&
+    usernameState.value == null
+)
 const login = () => {
   console.log(username.value, password.value)
 }
@@ -32,8 +38,6 @@ const login = () => {
       :state="passwordState"
       placeholder="your password"
     />
-    <button @click="login()">
-      Login
-    </button>
+    <ButtonField :disabled="!validForm" label="Submit" @click="login()" />
   </div>
 </template>
