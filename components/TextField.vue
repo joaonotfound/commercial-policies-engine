47567ab00c05f0eecb837f8b6a46d3060f905b95
joaonotfound <joaonotfound@protonmail.com>
2023-02-19 14:32:48 -0300

    style: update TextField style

--- components/TextField.vue
+++ components/TextField.vue
@@ -1,9 +1,10 @@
+<!-- eslint-disable vue/html-closing-bracket-newline -->
+<!-- eslint-disable vue/html-self-closing -->
 <template>
   <div class="mt-2">
     <label
       for="text-field"
-      class="text-md focus:border-blue text-dark-d2 mb-1 inline-block capitalize"
-    >
+      class="text-md focus:border-blue text-dark-d2 inline-block capitalize">
       {{ label }}
     </label>
     <input
@@ -11,23 +12,25 @@
       :placeholder="placeholder"
       :type="textType ?? 'text'"
       :class="`${state?.level}-input`"
-      class="transition rounded-md text-base py-2 pl-4 pr-2 ease-in-out text-dark-d2 block border border-solid border-light-d2 bg-light focus:bg-primary-l3 focus:border-primary-l1 focus:outline-none"
-    >
-    <p v-if="state" :class="state.level" class="">
+      :value="value"
+      class="transition rounded-sm text-base py-2 pl-4 pr-2 ease-in-out text-dark-d2 block border border-solid border-light-d2 bg-light focus:bg-primary-l3 focus:border-primary-l1 focus:outline-none"
+      @input="$emit('update:value', ($event.target as any).value)" />
+    <p v-if="state" :class="state.level">
       {{ state?.message }}
     </p>
   </div>
 </template>
 
 <script setup lang="ts">
+import { InputState } from '../composables'
+
+defineEmits(['update:value'])
 defineProps<{
   textType?: string
   placeholder?: string
   label?: string
-  state?: {
-    level: 'success' | 'warning' | 'error'
-    message: string
-  }
+  value: any
+  state?: InputState | null
 }>()
 </script>
 <style scoped>
@@ -46,11 +49,11 @@ defineProps<{
 .warning,
 .error,
 .success {
-  @apply ml-1 capitalize;
+  @apply ml-1 text-sm mt-1;
 }
 
 .error {
-  @apply text-error;
+  @apply text-error placeholder:text-error-d2;
 }
 
 .success {
