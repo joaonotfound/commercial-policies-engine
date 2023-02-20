542367b0386e177f3d5aaac911316691084ba92b
joaonotfound <joaonotfound@protonmail.com>
2023-02-20 01:37:06 -0300

    chore: update dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,20 @@
 module.exports = {
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js',
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/$1'
+    // '^@/(.*)$': '<rootDir>/$1',
+    // '^~/(.*)$': '<rootDir>/$1'
+    // '^vue$': 'vue/dist/vue.common.js'
   },
-  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
-    '^.+\\.js$': 'babel-jest',
+    '^.+\\.js$': 'babel-jest'
   },
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
   ],
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jsdom'
 }
