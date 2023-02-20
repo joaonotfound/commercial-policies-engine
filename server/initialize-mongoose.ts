0f539a796be53b9917a29e0d94a94719791a3bef
joaonotfound <joaonotfound@protonmail.com>
2023-02-20 01:45:16 -0300

    chore: setup mongoose config

---
+++ server/initialize-mongoose.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const config = useRuntimeConfig()
+export default async () => {
+  try {
+    await mongoose.connect(config.mongoUrl)
+    console.log('DB connection established.')
+  } catch (err) {
+    console.error('DB connection failed.', err)
+  }
+}
