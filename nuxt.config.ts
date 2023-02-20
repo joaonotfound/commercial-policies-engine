46f522769709e664c0d9f9205ea97a0ee8ef98c5
joaonotfound <joaonotfound@protonmail.com>
2023-02-20 01:46:12 -0300

    chore: setup mongo container

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['@fortawesome/fontawesome-svg-core/styles.css']
+  css: ['@fortawesome/fontawesome-svg-core/styles.css'],
+  runtimeConfig: {
+    mongoUrl: process.env.MONGO_URL
+  },
+  nitro: {
+    plugins: ['~/server/initialize-mongoose.ts']
+  }
 })
