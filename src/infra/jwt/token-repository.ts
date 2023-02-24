5990914326e74fccb9a6717e7364169ea98017db
joaonotfound <joaonotfound@protonmail.com>
2023-02-24 18:44:23 -0300

    chore: remove eslint false flag

--- src/infra/jwt/token-repository.ts
+++ src/infra/jwt/token-repository.ts
@@ -6,6 +6,7 @@ export class TokenRepository implements GenerateAccessToken {
   private readonly salt = 'super-secure-salt'
   // eslint-disable-next-line require-await
   async generateAccessToken(account: Account): Promise<string> {
+    // eslint-disable-next-line import/no-named-as-default-member
     return jwt.sign({ id: account }, this.salt)
   }
 }
