7252752229595f2d4916bbc6cb684c558efd79bb
joaonotfound <joaonotfound@protonmail.com>
2023-02-25 20:57:37 -0300

    test: should return public account

--- tests/data/usecases/db-add-account.test.ts
+++ tests/data/usecases/db-add-account.test.ts
@@ -1,17 +1,23 @@
 import { MockFindAccountByUsername } from '../mocks'
 import { PublicAccount, RegisterAccount, Result } from '@/domain'
-import { mockRegisterAccount } from '@/tests/domain'
-import { error, FindAccountByUsername } from '@/data'
+import { mockAccount, mockRegisterAccount } from '@/tests/domain'
+import { ok, error, FindAccountByUsername } from '@/data'
 
 export class DatabaseAddAccount {
   // eslint-disable-next-line no-useless-constructor
   constructor(private readonly findAccountByUsername: FindAccountByUsername) {}
-   
+
   async addAccount(
     account: RegisterAccount
   ): Promise<Result<PublicAccount, string>> {
-    await this.findAccountByUsername.findAccountByUsername(account.username)
-    return error('username already in use.')
+    const [existentAccount] =
+      await this.findAccountByUsername.findAccountByUsername(account.username)
+    if (existentAccount !== undefined) {
+      return error('username already in use.')
+    }
+    return ok({
+      username: account.username
+    })
   }
 }
 
@@ -23,11 +29,22 @@ const makeSut = () => {
 
 describe('DatabaseAddAccount', () => {
   test('should return account with username already exists.', async () => {
-    const { sut } = makeSut()
+    const { sut, findAccountByUsername } = makeSut()
+    findAccountByUsername.mockFindAccountByUsername([mockAccount()])
 
     const response = await sut.addAccount(mockRegisterAccount())
 
     expect(response).toEqual(error('username already in use.'))
+  })
+  test('should return public account.', async () => {
+    const { sut } = makeSut()
+    const mockedAccount = mockRegisterAccount()
+
+    const response = await sut.addAccount(mockedAccount)
+
+    expect(response).toEqual(
+      ok<PublicAccount>({ username: mockedAccount.username })
+    )
   })
   test('should call find account by username', async () => {
     const { sut, findAccountByUsername } = makeSut()
