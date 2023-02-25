48a3ba5861932edc8dbe08171e0a5610c2496879
joaonotfound <joaonotfound@protonmail.com>
2023-02-25 02:01:47 -0300

    feat: create homepage

---
+++ src/view/components/homepage/FeatureComponent.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+defineProps<{
+  title: string
+  description: string
+  svg: string
+}>()
+</script>
+
+<template>
+  <div
+    class="rounded-md m-4 hover:shadow-md shadow-sm px-4 py-8 bg-light text-dark flex flex-col justify-between items-center md:w-[21rem] lg:w-[20rem] h-[100%]">
+    <img :src="svg" width="150" />
+    <div class="flex flex-col items-center">
+      <p class="font-bold text-dark-d2 mb-1 text-primary-d2/80 text-lg">
+        {{ title }}
+      </p>
+      <p class="text-sm text-center">{{ description }}</p>
+    </div>
+
+    <button
+      class="bg-primary-d2/90 hover:bg-primary-d2 rounded-xl px-6 py-2 text-light font-bold shadow-lg outline-none">
+      More
+    </button>
+  </div>
+</template>
---
+++ src/view/components/homepage/FeaturesSection.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import FeatureComponent from './FeatureComponent.vue'
+</script>
+
+<template>
+  <div
+    class="bg-gradient-to-bl from-primary-d2/20 to-primary-d2/30 p-10 flex flex-col items-center">
+    <p class="text-primary/70 font-light text-xl mb-2 uppercase">Features</p>
+    <h2 class="text-primary-d1/70 font-bold text-3xl mb-12">
+      Our Features and Services.
+    </h2>
+    <div
+      class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-4">
+      <FeatureComponent
+        svg="/svg/fast-feature.svg"
+        title="Streamlined Interaction"
+        description="Efficiently manage business rules and policies for optimized interactions with customers and suppliers." />
+      <FeatureComponent
+        svg="/svg/custom-feature.svg"
+        title="Custom Validation"
+        description="Easily customize validation parameters and create validation logic using JavaScript codes for increased flexibility." />
+      <FeatureComponent
+        svg="/svg/integration-feature.svg"
+        title="Seamless Integration"
+        description="Seamlessly integrate with third-party systems for obtaining value ranges and billing history validation." />
+    </div>
+
+    <a
+      href="https://storyset.com/people"
+      class="text-primary-d2/80 mt-16 hover:underline"
+      >People illustrations by Storyset</a
+    >
+  </div>
+</template>
