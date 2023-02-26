1740acf2639547c412cc8f8c654ddac0b2d20e47
joaonotfound <joaonotfound@protonmail.com>
2023-02-25 22:07:49 -0300

    feat: class MongooseAccountRepository implements FindAccountsByEmail

--- src/infra/mongoose/mongoose-account-repository.ts
+++ src/infra/mongoose/mongoose-account-repository.ts
@@ -1,9 +1,16 @@
 import { accountSchema } from './schemas'
-import { FindAccountByUsername } from '@/data'
+import { FindAccountByUsername, FindAccountsByEmail } from '@/data'
 import { Account } from '@/domain'
 
-export class MongooseAccountRepository implements FindAccountByUsername {
+export class MongooseAccountRepository
+  implements FindAccountByUsername, FindAccountsByEmail
+{
   async findAccountByUsername(username: string): Promise<Account[]> {
     return await accountSchema.find({ username })
+  }
+
+  async findAccountByEmail(email: string): Promise<Account | null> {
+    const [account] = await accountSchema.find({ email })
+    return account === undefined ? null : (account as Account)
   }
 }
--- src/infra/mongoose/schemas/account.schema.ts
+++ src/infra/mongoose/schemas/account.schema.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 const schema = new mongoose.Schema(
   {
     username: { type: String, unique: true },
+    email: { type: String, unique: true },
     password: { type: String }
   },
   { timestamps: true, strict: true, strictQuery: true }
