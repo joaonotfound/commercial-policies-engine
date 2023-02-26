f85e5dc2b96e549ae44e2c925000c860840cd50c
joaonotfound <joaonotfound@protonmail.com>
2023-02-25 22:14:09 -0300

    chore: class MongooseAccountRepository implements AddAccountRepository

--- src/infra/mongoose/mongoose-account-repository.ts
+++ src/infra/mongoose/mongoose-account-repository.ts
@@ -1,16 +1,37 @@
 import { accountSchema } from './schemas'
-import { FindAccountByUsername, FindAccountsByEmail } from '@/data'
-import { Account } from '@/domain'
+import {
+  AddAccountRepository,
+  FindAccountByUsername,
+  FindAccountsByEmail
+} from '@/data'
+import { Account, RegisterAccount } from '@/domain'
 
 export class MongooseAccountRepository
-  implements FindAccountByUsername, FindAccountsByEmail
+  implements FindAccountByUsername, FindAccountsByEmail, AddAccountRepository
 {
+  async addAccount(account: RegisterAccount): Promise<boolean> {
+    try {
+      await accountSchema.create(account)
+      return true
+    } catch {
+      return false
+    }
+  }
+
   async findAccountByUsername(username: string): Promise<Account[]> {
-    return await accountSchema.find({ username })
+    try {
+      return await accountSchema.find({ username })
+    } catch {
+      return []
+    }
   }
 
   async findAccountByEmail(email: string): Promise<Account | null> {
-    const [account] = await accountSchema.find({ email })
-    return account === undefined ? null : (account as Account)
+    try {
+      const [account] = await accountSchema.find({ email })
+      return account === undefined ? null : (account as Account)
+    } catch {
+      return null
+    }
   }
 }
